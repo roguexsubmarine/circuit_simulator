863942706e7f9bdc7d2b956d17d870fbedffc486
“roguexsubmarine” <roguexsubmarine@gmail.com>
2024-04-16 18:24:44 +0530

    node added

--- circuit_sim.py
+++ circuit_sim.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QRadioButton, QHBoxLayout, QGridLayout, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QRadioButton, QHBoxLayout, QGridLayout, QPushButton, QLineEdit
 from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtGui import QIcon, QPixmap, QTransform
 
@@ -10,27 +10,40 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("PassiveElectronFlowAnalyser")
 
         leftPanel = QVBoxLayout()
-        # Create radio buttons
+
+        self.inputValue = QLineEdit()
+        self.inputValue.setMaxLength(5)
+        self.inputValue.setPlaceholderText("Enter your Value")
+        # self.inputValue.returnPressed.connect(self.return_pressed)  # Connect to method in MainWindow
+        # self.inputValue.selectionChanged.connect(self.selection_changed)
+        # self.inputValue.textChanged.connect(self.text_changed)
+        # self.inputValue.textEdited.connect(self.text_edited)
+
+
+        ### Create radio buttons
         self.resistor_button = QRadioButton("Resistor")
         self.battery_button = QRadioButton("Battery")
-        self.inductor_button = QRadioButton("Inductor")
         self.wire_button = QRadioButton("Wire")
+        self.node_button = QRadioButton("Node")
         
-        # Set "Wire" radio button as default selected
+        ## Set "Wire" radio button as default selected
         self.wire_button.setChecked(True)
 
-        # Connect radio buttons to slot
+        ## Connect radio buttons to slot
         self.resistor_button.clicked.connect(self.radio_button_clicked)
         self.battery_button.clicked.connect(self.radio_button_clicked)
-        self.inductor_button.clicked.connect(self.radio_button_clicked)
         self.wire_button.clicked.connect(self.radio_button_clicked)
+        self.node_button.clicked.connect(self.radio_button_clicked)
 
+
+        ### Adding components to left Panel 
+        leftPanel.addWidget(self.inputValue)
         leftPanel.addWidget(self.resistor_button)
         leftPanel.addWidget(self.battery_button)
-        leftPanel.addWidget(self.inductor_button)
         leftPanel.addWidget(self.wire_button)
+        leftPanel.addWidget(self.node_button)
 
-        # Create a "Clear" button
+        ## Create a "Clear" button
         clear_button = QPushButton("Clear")
         clear_button.clicked.connect(self.clear_buttons)
         leftPanel.addWidget(clear_button)
@@ -39,7 +52,7 @@ class MainWindow(QMainWindow):
         rightGrid.setSpacing(0)
         rightGrid.setContentsMargins(0, 0, 0, 0)
         
-        ##number of column and rows of the grid
+        ## number of column and rows of the grid
         n = 15
         self.buttons = []  # List to hold all buttons
         self.button_rotations = {}  # Dictionary to store rotation angle for each button
@@ -62,8 +75,13 @@ class MainWindow(QMainWindow):
         widget.setLayout(outerLayout)
         self.setCentralWidget(widget)
 
-        # Keep track of the active component
+        ## Keep track of the active component
         self.active_component = "Wire"
+
+    def return_pressed(self):
+        # This method will be called when the return key is pressed in the QLineEdit
+        text = self.inputValue.text()
+        print("Return Pressed:", text)
 
     def radio_button_clicked(self):
         sender = self.sender()
@@ -78,7 +96,7 @@ class MainWindow(QMainWindow):
         icon_path = f"./icons/{self.active_component.lower()}.jpg"
         pixmap = QPixmap(icon_path)
 
-        # Rotate the image
+        ## Rotate the image
         rotation = self.button_rotations[button]
         rotation += 90
         if rotation >= 360:
@@ -86,11 +104,11 @@ class MainWindow(QMainWindow):
         transform = QTransform().rotate(rotation)
         rotated_pixmap = pixmap.transformed(transform)
 
-        # Set the rotated icon for the button
+        ## Set the rotated icon for the button
         button.setIcon(QIcon(rotated_pixmap))
         button.setIconSize(QSize(30, 30))
 
-        # Update the rotation angle for the button
+        ## Update the rotation angle for the button
         self.button_rotations[button] = rotation
 
     def clear_buttons(self):
@@ -99,6 +117,8 @@ class MainWindow(QMainWindow):
             button.setIcon(QIcon())  # Clear the icon
             self.button_rotations[button] = 0  # Reset rotation angle to 0
         print("Buttons Cleared")
+
+
 
 
 app = QApplication(sys.argv)
